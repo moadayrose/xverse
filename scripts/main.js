6e3ee756114cfce917c163e4d2795e6ae83a910b
moadayrose <158135262+moadayrose@users.noreply.github.com>
2024-01-29 23:14:17 -0800

    Change URL

--- scripts/main.js
+++ scripts/main.js
@@ -367,7 +367,7 @@ function removeRankedTrainee(trainee) {
   return false;
 }
 
-const currentURL = "https://produce48.github.io/";
+const currentURL = "https://moadayrose.github.io/xverse/";
 // Serializes the ranking into a string and appends that to the current URL
 function generateShareLink() {
   let shareCode = ranking.map(function (trainee) {
